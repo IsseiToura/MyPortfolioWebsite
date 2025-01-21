35ecd9e0db417aec2384f404f78c9ecbc19fd261
Issei Toura <isseitoura@IsseinoMac-mini.local>
2025-01-21 13:03:29 +1000

    correct the name of file

--- isseipage/data/skills.py
+++ isseipage/data/skills.py
@@ -1,6 +1,6 @@
 def get_skills():
     return [
-        {"name": "C#", "image": "C#.png"},
+        {"name": "C#", "image": "Csharp.png"},
         {"name": "Python", "image": "Python.png"},
         {"name": "TypeScript", "image": "TypeScript.png"},
         {"name": "React", "image": "React.png"}
