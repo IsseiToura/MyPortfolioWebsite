ba237a77d2912d3de777fef08a11cd84315e3510
IsseiToura <isseitoura0429@gmail.com>
2025-06-19 17:25:52 +1000

    Correct some points 2025/06/19

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MyPortfolioWebsite
+
+Welcome to MyPortfolioWebsite! This is a personal portfolio website designed to showcase my projects, skills, and experience in a modern and visually appealing way by using Python Flask.
+
+## Features
+
+- Responsive design for all devices
+- Project showcase section
+- About me section
+- Contact form
+- Easy customization
--- isseipage/data/projects.py
+++ isseipage/data/projects.py
@@ -1,6 +1,6 @@
 def get_projects():
-    return [
-        {"id": 1, "image": "linebot.png", "title": "Daily Bot for AUD/JPY Excange Rates and English Vocabulary",
+    projects = [
+        {"id": 1, "image": "linebot.png", "title": "Daily Bot for AUD/JPY Rate and English Vocabulary",
          "description": "1. Developed a bot that retrieves AUD/JPY exchange rates and English vocabulary words daily from APIs and stores in a databese." \
                         "\n\n2. The bot sends this information to a messaging app automatically once a day." \
                         "\n\n3. Built a vocabulary learning app that displays a list of vocabulary words stored in a database. Users can also add their own words they want to learn."\
@@ -19,5 +19,24 @@ def get_projects():
          "description": "1. Created a personal portfolio website to showcase my work as a software developer, using Python Flask." \
                         "\n\n2. Utilized Bootstrap to ensure responsive design, making the website compatible with various screen sizes, including PCs, tablets, and smartphones.",
          "environment": " - Language: Python, HTML, CSS\n - Others: Python Flask, BootStrap",
-         "url": "https://github.com/IsseiToura/MyPortfolioWebsite"}
+         "url": "https://github.com/IsseiToura/MyPortfolioWebsite"},
+
+         {"id": 4, "image": "VocabMaster.png", "title": "Vocabulary Master",
+         "description": "1. Uses the OpenAI API to automatically generate English words tailored to your selected IELTS level, including pronunciation, Japanese translations, and example sentences." \
+                        "\n\n2. Lets you build your own vocabulary list by adding only the words you want to learn." \
+                        "\n\n3. Includes a flashcard practice feature that shows 10 random words from your list so you can review them interactively." \
+                        "\n\n4. Tracks your practice history, showing which words you got right or wrong to help you focus on improvement.",
+         "environment": " - Language: JavaScript\n - DB: MongoDB\n - Others: Node.js, OpenAI API",
+         "url": "https://www.linkedin.com/posts/issei-toura-1502851a1_ielts-englishlearning-reactjs-activity-7334758600831025152-maV_?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC80UowB-h4CtQwXDHw9lxwFfPUT2oHqn_Y"},
+
+         {"id": 5, "image": "Forensic.png", "title": "Forensic Data Visualizer",
+         "description": "1. Implemented automated PDF parsing and information extraction." \
+                        "\n\n2. Cross-document entity matching using keys such as ACNs and full names." \
+                        "\n\n3. Auto-generated relationship diagrams, and structured data export to Excel for further analysis." \
+                        "\n\n4. This was a collaborative project, and my role was project manager and developer.",
+         "environment": " - Language: Python and TypeScript\n - Others: Python Flask, PDFPlumber, React",
+         "url": "https://www.linkedin.com/posts/issei-toura-1502851a1_as-part-of-my-university-curriculum-i-had-activity-7339480483979776002-QgEZ?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC80UowB-h4CtQwXDHw9lxwFfPUT2oHqn_Y"}
     ]
+    
+    # IDを逆順（降順）でソートして返す
+    return sorted(projects, key=lambda x: x["id"], reverse=True)
